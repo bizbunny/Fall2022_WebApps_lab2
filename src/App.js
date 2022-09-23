120fd34bb2aed92a976f5bf51552a1916e39ce6f
Vi-Anh Nguyen <41245033+bizbunny@users.noreply.github.com>
2022-09-23 10:28:11 -0500

    minor updates and some inclusion of css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import Login from "./components/Login";
+import "./styles/App.css";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
+      <Login />
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,7 @@ export default function Login(){
         <form onSubmit= {e => e.preventDefault ()}>
             <label htmlFor="login-username">Username: </label>
             <input type="text" name="login-username" id="login-username" />
-            <label htmlFor="login-password">Password: </label>
+            <label htmlFor="login-password"> Password: </label>
             <input type="password" name="login-password" id="login-password" />
             <input type="submit" value="Login" />
         </form>
