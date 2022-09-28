9ea302d180906ec58026868dc5c955c916e659d1
Vi-Anh Nguyen <41245033+bizbunny@users.noreply.github.com>
2022-09-28 09:18:27 -0500

    minor updates

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import {useState} from 'react';
 import CreateTodo from "./components/CreateTodo";
 function App() {
   const [user, setUser] = useState('')
+  const initialPosts = [
+    {
+      title:"",
+      content:"",
+      author:""
+    }
+  ]
+  const [todo, setTodo] = useState(initialPosts)
   return (
     <div className="App">
       <header className="App-header">
       </header>
-      <UserBar user={user && <CreateTodo user={user} />} setUser={setUser} />
+      <UserBar user={user && <CreateTodo user={user} todo={todo} setTodo={setTodo}/>} setUser={setUser} />
       <Todolist />
     </div>
   );
