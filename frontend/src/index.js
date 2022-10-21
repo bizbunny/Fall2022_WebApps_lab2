61eb299ca53812bbbc980f4bac7c063c89726251
Vi-Anh Nguyen <41245033+bizbunny@users.noreply.github.com>
2022-10-21 09:24:05 -0500

    import in axios library stuff

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { RequestProvider } from "react-request-hook";
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:3000/api/",
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <RequestProvider value={axiosInstance}>
     <App />
-  </React.StrictMode>
+  </RequestProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
