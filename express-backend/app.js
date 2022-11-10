60293217d70dd5eeb33d36e4e710ccb11579517a
Anh Nguyen <vianhnguyen17@gmail.com>
2022-11-09 22:37:07 -0600

    express prep updates

---
+++ express-backend/app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// route definition
+// app.get('/', function (req, res) {
+// res.send('Hello World');
+// });
+// Start server
+// app.listen(4000, function () {
+// console.log('App listening at Port 4000..');
+// });
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
